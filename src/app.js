b9f1f8c6e21558b628c9d2a6cf65829ab311e5fe
pedsm <peumendonca@gmail.com>
2018-03-10 21:17:36 +0000

    Default text changed

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ app.use((req, res, next) => {
 app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
-    res.send('hello')
+    res.send('Night Owls')
 })
 
 app.post('/user', async (req, res) => {
